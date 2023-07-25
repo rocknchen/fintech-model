6c6605d342ebfef7b635cafd787f559fe7f1c2d9
rockchen <rochchen@htsc.com>
2023-07-25 20:59:25 +0800

    update

--- src/main/java/com/hthk/fintech/model/event/EventTrade.java
+++ src/main/java/com/hthk/fintech/model/event/EventTrade.java
@@ -1,4 +1,35 @@
 package com.hthk.fintech.model.event;
 
+import com.hthk.fintech.enumration.EventTypeTradeEnum;
+
+import java.time.LocalDateTime;
+
 public class EventTrade implements IEvent {
+
+    private String domain;
+
+    private EventTypeTradeEnum type;
+
+    private LocalDateTime dateTime;
+
+    public EventTrade() {
+    }
+
+    public EventTrade(String domain, EventTypeTradeEnum type, LocalDateTime dateTime) {
+        this.domain = domain;
+        this.type = type;
+        this.dateTime = dateTime;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public EventTypeTradeEnum getType() {
+        return type;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
 }
