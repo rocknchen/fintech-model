45d95980bc9f6bf4463101cc3f1eb6df4c4e0db6
rockchen <rochchen@htsc.com>
2023-07-27 15:57:38 +0800

    update

--- src/main/java/com/hthk/fintech/enumration/CSVField.java
+++ src/main/java/com/hthk/fintech/enumration/CSVField.java
@@ -1,5 +1,6 @@
 package com.hthk.fintech.enumration;
 
+import com.hthk.fintech.converter.AttributeStringConverter;
 import com.hthk.fintech.converter.impl.AttStringDefaultConverter;
 
 import java.lang.annotation.ElementType;
@@ -13,6 +14,6 @@ public @interface CSVField {
 
     String header();
 
-    Class<?> converter() default AttStringDefaultConverter.class;
+    Class<? extends AttributeStringConverter> converter() default AttStringDefaultConverter.class;
 
 }
