f6451ef726b4554f33eb4e59c6b0e3221b1d7337
rockchen <rochchen@htsc.com>
2023-07-27 19:33:53 +0800

    update

--- src/main/java/com/hthk/fintech/converter/AttributeStringConverter.java
+++ src/main/java/com/hthk/fintech/converter/AttributeStringConverter.java
@@ -3,7 +3,7 @@ package com.hthk.fintech.converter;
 public interface AttributeStringConverter<T> {
 
     default String process(T input) {
-        return input.toString();
+        return input == null ? null : input.toString();
     }
 
 }
