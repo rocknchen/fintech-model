e7b3c0ab7c81c0cfb12adc896efb0746344cb7ab
rockchen <rochchen@htsc.com>
2023-07-27 19:58:02 +0800

    update

--- src/main/java/com/hthk/fintech/model/event/EventCriteria.java
+++ src/main/java/com/hthk/fintech/model/event/EventCriteria.java
@@ -1,4 +1,16 @@
 package com.hthk.fintech.model.event;
 
+import java.time.LocalDateTime;
+
 public class EventCriteria {
+
+    private LocalDateTime eventTime;
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(LocalDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
 }
