c34b206cbe02247533016bf9619628a9fb43f2cc
rockchen <rochchen@htsc.com>
2023-07-27 09:31:18 +0800

    update

--- src/main/java/com/hthk/fintech/enumration/DateTimeFormatEnum.java
+++ src/main/java/com/hthk/fintech/enumration/DateTimeFormatEnum.java
@@ -1,12 +1,5 @@
 package com.hthk.fintech.enumration;
 
-import org.apache.commons.collections.map.HashedMap;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public enum DateTimeFormatEnum {
 
     YYYYMM("yyyyMM");
@@ -15,14 +8,6 @@ public enum DateTimeFormatEnum {
 
     DateTimeFormatEnum(String format) {
         this.format = format;
-    }
-
-    public static Map<String, DateTimeFormatEnum> getFormatMap() {
-
-        Map<String, DateTimeFormatEnum> formatMap = new HashedMap();
-        List<DateTimeFormatEnum> formatList = Arrays.stream(values()).collect(Collectors.toList());
-        formatList.forEach(t -> formatMap.put(t.format, t));
-        return formatMap;
     }
 
     public String getFormat() {
