d9e399bede11837d89dedee8b260110b28b48b30
rockchen <rochchen@htsc.com>
2023-07-28 17:39:18 +0800

    update

--- src/main/java/com/hthk/common/utils/CustomStringUtils.java
+++ src/main/java/com/hthk/common/utils/CustomStringUtils.java
@@ -3,7 +3,7 @@ package com.hthk.common.utils;
 public class CustomStringUtils {
 
     public static boolean hasText(String value) {
-        return value != null || !value.trim().equals("");
+        return value != null && !value.trim().equals("");
     }
 
 }
