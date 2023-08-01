a8745f35489be6cad1ed871aa136643e907e4816
rockchen <rochchen@htsc.com>
2023-08-01 16:43:11 +0800

    update

--- src/main/java/com/hthk/fintech/model/event/EventProcessEntity.java
+++ src/main/java/com/hthk/fintech/model/event/EventProcessEntity.java
@@ -1,4 +1,38 @@
 package com.hthk.fintech.model.event;
 
+import com.hthk.fintech.enumration.EventProcessStatusEnum;
+
+import java.time.LocalDateTime;
+
 public class EventProcessEntity {
+
+    private String eventId;
+
+    private EventProcessStatusEnum status;
+
+    private LocalDateTime updatedDateTime;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public EventProcessStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(EventProcessStatusEnum status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public void setUpdatedDateTime(LocalDateTime updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
 }
