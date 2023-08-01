671e85559151646417384a5fb66abc1461df2181
rockchen <rochchen@htsc.com>
2023-08-01 16:36:45 +0800

    update

--- src/main/java/com/hthk/fintech/model/event/EventCriteria.java
+++ src/main/java/com/hthk/fintech/model/event/EventCriteria.java
@@ -4,7 +4,17 @@ import java.time.LocalDateTime;
 
 public class EventCriteria {
 
+    private String domain;
+
     private LocalDateTime eventTime;
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 
     public LocalDateTime getEventTime() {
         return eventTime;
