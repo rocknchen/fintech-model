4aad185b6701b14736328a2e767e2a3a79127490
rockchen <rochchen@htsc.com>
2023-08-01 16:38:27 +0800

    update

--- src/main/java/com/hthk/fintech/model/event/EventCriteria.java
+++ src/main/java/com/hthk/fintech/model/event/EventCriteria.java
@@ -1,12 +1,24 @@
 package com.hthk.fintech.model.event;
 
+import com.hthk.fintech.enumration.EventGroupEnum;
+
 import java.time.LocalDateTime;
 
 public class EventCriteria {
 
     private String domain;
 
+    private EventGroupEnum group;
+
     private LocalDateTime eventTime;
+
+    public EventGroupEnum getGroup() {
+        return group;
+    }
+
+    public void setGroup(EventGroupEnum group) {
+        this.group = group;
+    }
 
     public String getDomain() {
         return domain;
