c863051fda8e833b9975e1d52e3465d11a607eb9
rockchen <rochchen@htsc.com>
2023-08-10 14:38:46 +0800

    update

--- src/main/java/com/hthk/fintech/model/quote/dto/QuoteCSVDTO.java
+++ src/main/java/com/hthk/fintech/model/quote/dto/QuoteCSVDTO.java
@@ -5,8 +5,10 @@ import com.hthk.fintech.enumration.CSVField;
 import com.hthk.fintech.enumration.FieldOrder;
 
 @FieldOrder({"date", "instrumentGroup", "basicInstrumentGroup", "securityGroup",
-        "name", "isin", "cusip", "bbTicker",
-        "quoteType", "close"
+        "name", "isin", "cusip", "bbTicker", "source",
+        "quoteType", "close",
+        "enteredUser", "enteredDateTime",
+        "updatedUser", "updatedDateTime"
 })
 public class QuoteCSVDTO {
 
@@ -34,11 +36,66 @@ public class QuoteCSVDTO {
     @CSVField(header = "BB Ticker")
     private String bbTicker;
 
+    @CSVField(header = "Source")
+    private String source;
+
     @CSVField(header = "Quote Type")
     private String quoteType;
 
     @CSVField(header = "Close")
     private String close;
+
+    @CSVField(header = "Entered User")
+    private String enteredUser;
+
+    @CSVField(header = "Entered Date Time")
+    private String enteredDateTime;
+
+    @CSVField(header = "Updated User")
+    private String updatedUser;
+
+    @CSVField(header = "Updated Date Time")
+    private String updatedDateTime;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getEnteredUser() {
+        return enteredUser;
+    }
+
+    public void setEnteredUser(String enteredUser) {
+        this.enteredUser = enteredUser;
+    }
+
+    public String getEnteredDateTime() {
+        return enteredDateTime;
+    }
+
+    public void setEnteredDateTime(String enteredDateTime) {
+        this.enteredDateTime = enteredDateTime;
+    }
+
+    public String getUpdatedUser() {
+        return updatedUser;
+    }
+
+    public void setUpdatedUser(String updatedUser) {
+        this.updatedUser = updatedUser;
+    }
+
+    public String getUpdatedDateTime() {
+        return updatedDateTime;
+    }
+
+    public void setUpdatedDateTime(String updatedDateTime) {
+        this.updatedDateTime = updatedDateTime;
+    }
 
     public String getDate() {
         return date;
