28f151943d30616a8bda00906feb9048c102b5c6
rockchen <rochchen@htsc.com>
2023-08-10 14:50:07 +0800

    update

---
+++ src/main/java/com/hthk/external/calypso/CalypsoEODQuoteStaticData.java
@@ -0,0 +1,14 @@
+package com.hthk.external.calypso;
+
+public class CalypsoEODQuoteStaticData {
+
+    public static final String KW_ATTR_CALYPSO_EOD_QUOTE_ENTERED_DATE_TIME = "Calypso EOD Quote Entered Date Time";
+
+    public static final String KW_ATTR_CALYPSO_EOD_QUOTE_ENTERED_USER = "Calypso EOD Quote Entered User";
+
+    public static final String KW_ATTR_CALYPSO_EOD_QUOTE_UPDATED_DATE_TIME = "Calypso EOD Quote Updated Date Time";
+
+    public static final String KW_ATTR_CALYPSO_EOD_QUOTE_UPDATED_USER = "Calypso EOD Quote Updated User";
+
+
+}
