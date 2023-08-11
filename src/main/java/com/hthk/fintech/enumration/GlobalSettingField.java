4898d8b07d537b62dcffa6b23ea506b72e3f2bb3
rockchen <rochchen@htsc.com>
2023-08-11 14:09:59 +0800

    update

--- src/main/java/com/hthk/fintech/enumration/GlobalSettingField.java
+++ src/main/java/com/hthk/fintech/enumration/GlobalSettingField.java
@@ -5,8 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
 public @interface GlobalSettingField {
 
     String name();
