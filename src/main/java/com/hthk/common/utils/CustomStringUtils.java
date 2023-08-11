51feb3694c315a870cceeca0877666016a8e5226
rockchen <rochchen@htsc.com>
2023-08-11 11:13:32 +0800

    update

--- src/main/java/com/hthk/common/utils/CustomStringUtils.java
+++ src/main/java/com/hthk/common/utils/CustomStringUtils.java
@@ -1,11 +1,18 @@
 package com.hthk.common.utils;
 
+import java.util.Map;
+
 public class CustomStringUtils {
 
     public static boolean hasText(String value) {
         return value != null && !value.trim().equals("");
     }
 
-
+    public static String replaceAll(String path, Map<String, String> replaceMap) {
+        for (String key : replaceMap.keySet()) {
+            path = path.replaceAll(key, replaceMap.get(key));
+        }
+        return path;
+    }
 
 }
