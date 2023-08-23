98f19d54bb7ae82aaaff128de09f962ada9afecd
rockchen <rochchen@htsc.com>
2023-08-23 11:33:39 +0800

    update

--- src/main/java/com/hthk/calypsox/model/trade/dto/TradeCashFlowCalCSVDTO.java
+++ src/main/java/com/hthk/calypsox/model/trade/dto/TradeCashFlowCalCSVDTO.java
@@ -4,14 +4,129 @@ import com.hthk.fintech.enumration.CSVField;
 import com.hthk.fintech.enumration.FieldOrder;
 
 @FieldOrder({
-        "tradeId", "productKey",
+        "book", "productType", "productSubType",
+        "fixedFloat",
+        "fixedRate",
+        "rateIndex", "rateIndexTenor", "spread",
+        "origTradeId", "tradeId", "tradeKeywordTradeIdLink",
+        "productKey",
         "settlementType", "payRec", "currency",
         "amount", "date"
 })
 public class TradeCashFlowCalCSVDTO {
 
+    @CSVField(header = "Book")
+    private String book;
+
+    @CSVField(header = "Product Type")
+    private String productType;
+
+    @CSVField(header = "Product Sub Type")
+    private String productSubType;
+
+    @CSVField(header = "Fixed or Floating")
+    private String fixedFloat;
+
+    @CSVField(header = "Fixed Rate")
+    private String fixedRate;
+
+    @CSVField(header = "Rate Index")
+    private String rateIndex;
+
+    @CSVField(header = "Rate Index Tenor")
+    private String rateIndexTenor;
+
+    @CSVField(header = "Spread")
+    private String spread;
+
+    @CSVField(header = "Orig Trade Id")
+    private String origTradeId;
+
     @CSVField(header = "Trade Id")
     private String tradeId;
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductSubType() {
+        return productSubType;
+    }
+
+    public void setProductSubType(String productSubType) {
+        this.productSubType = productSubType;
+    }
+
+    public String getFixedFloat() {
+        return fixedFloat;
+    }
+
+    public void setFixedFloat(String fixedFloat) {
+        this.fixedFloat = fixedFloat;
+    }
+
+    public String getFixedRate() {
+        return fixedRate;
+    }
+
+    public void setFixedRate(String fixedRate) {
+        this.fixedRate = fixedRate;
+    }
+
+    public String getRateIndex() {
+        return rateIndex;
+    }
+
+    public void setRateIndex(String rateIndex) {
+        this.rateIndex = rateIndex;
+    }
+
+    public String getRateIndexTenor() {
+        return rateIndexTenor;
+    }
+
+    public void setRateIndexTenor(String rateIndexTenor) {
+        this.rateIndexTenor = rateIndexTenor;
+    }
+
+    public String getSpread() {
+        return spread;
+    }
+
+    public void setSpread(String spread) {
+        this.spread = spread;
+    }
+
+    public String getOrigTradeId() {
+        return origTradeId;
+    }
+
+    public void setOrigTradeId(String origTradeId) {
+        this.origTradeId = origTradeId;
+    }
+
+    public String getTradeKeywordTradeIdLink() {
+        return tradeKeywordTradeIdLink;
+    }
+
+    public void setTradeKeywordTradeIdLink(String tradeKeywordTradeIdLink) {
+        this.tradeKeywordTradeIdLink = tradeKeywordTradeIdLink;
+    }
+
+    @CSVField(header = "Trade Keyword TRADE_ID_LINK")
+    private String tradeKeywordTradeIdLink;
 
     @CSVField(header = "Product Key")
     private String productKey;
