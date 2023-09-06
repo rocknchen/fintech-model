1311ec6398fe72535c5a664b7868a7800f01d17a
rockchen <rochchen@htsc.com>
2023-09-06 13:40:44 +0800

    update

--- src/main/java/com/hthk/calypsox/model/staticdata/dto/BookCalCSVDTO.java
+++ src/main/java/com/hthk/calypsox/model/staticdata/dto/BookCalCSVDTO.java
@@ -4,7 +4,11 @@ import com.hthk.fintech.enumration.CSVField;
 import com.hthk.fintech.enumration.FieldOrder;
 
 @FieldOrder({
-        "id", "name"
+        "id", "name", "legalEntity",
+        "companyShortName", "department",
+        "tradingDesk", "businessType", "business",
+        "baseCurrency",
+        "omsSystem", "bbTraderPortfolio", "risk"
 })
 public class BookCalCSVDTO {
 
@@ -13,6 +17,36 @@ public class BookCalCSVDTO {
 
     @CSVField(header = "Book Name")
     private String name;
+
+    @CSVField(header = "Legal Entity")
+    private String legalEntity;
+
+    @CSVField(header = "Base Currency")
+    private String baseCurrency;
+
+    @CSVField(header = "公司简称")
+    private String companyShortName;
+
+    @CSVField(header = "Department")
+    private String department;
+
+    @CSVField(header = "Trading Desk")
+    private String tradingDesk;
+
+    @CSVField(header = "交易性质")
+    private String businessType;
+
+    @CSVField(header = "业务类别")
+    private String business;
+
+    @CSVField(header = "源头系统")
+    private String omsSystem;
+
+    @CSVField(header = "BB_TRADER_PORTFOLIO")
+    private String bbTraderPortfolio;
+
+    @CSVField(header = "风险小组")
+    private String risk;
 
     public String getId() {
         return id;
@@ -28,5 +62,85 @@ public class BookCalCSVDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLegalEntity() {
+        return legalEntity;
+    }
+
+    public void setLegalEntity(String legalEntity) {
+        this.legalEntity = legalEntity;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public String getCompanyShortName() {
+        return companyShortName;
+    }
+
+    public void setCompanyShortName(String companyShortName) {
+        this.companyShortName = companyShortName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTradingDesk() {
+        return tradingDesk;
+    }
+
+    public void setTradingDesk(String tradingDesk) {
+        this.tradingDesk = tradingDesk;
+    }
+
+    public String getBusinessType() {
+        return businessType;
+    }
+
+    public void setBusinessType(String businessType) {
+        this.businessType = businessType;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
+    }
+
+    public String getOmsSystem() {
+        return omsSystem;
+    }
+
+    public void setOmsSystem(String omsSystem) {
+        this.omsSystem = omsSystem;
+    }
+
+    public String getBbTraderPortfolio() {
+        return bbTraderPortfolio;
+    }
+
+    public void setBbTraderPortfolio(String bbTraderPortfolio) {
+        this.bbTraderPortfolio = bbTraderPortfolio;
+    }
+
+    public String getRisk() {
+        return risk;
+    }
+
+    public void setRisk(String risk) {
+        this.risk = risk;
     }
 }
