915c02fafa1b37484821c3b629782deaa0f4e13e
rockchen <rochchen@htsc.com>
2023-09-06 11:01:00 +0800

    update

---
+++ src/main/java/com/hthk/calypsox/model/staticdata/dto/BookCalCSVDTO.java
@@ -0,0 +1,32 @@
+package com.hthk.calypsox.model.staticdata.dto;
+
+import com.hthk.fintech.enumration.CSVField;
+import com.hthk.fintech.enumration.FieldOrder;
+
+@FieldOrder({
+        "id", "name"
+})
+public class BookCalCSVDTO {
+
+    @CSVField(header = "Book Id")
+    private String id;
+
+    @CSVField(header = "Book Name")
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
