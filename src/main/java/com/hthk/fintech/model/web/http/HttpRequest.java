29cab0bfb9d31c5a639b3146947d7a763ec1771d
rockchen <rochchen@htsc.com>
2023-11-14 14:27:41 +0800

    update

---
+++ src/main/java/com/hthk/fintech/model/web/http/HttpRequest.java
@@ -0,0 +1,8 @@
+package com.hthk.fintech.model.web.http;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 14:15
+ */
+public class HttpRequest {
+}
---
+++ src/main/java/com/hthk/fintech/model/web/http/HttpResponse.java
@@ -0,0 +1,10 @@
+package com.hthk.fintech.model.web.http;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 14:15
+ */
+public class HttpResponse<R> {
+
+
+}
