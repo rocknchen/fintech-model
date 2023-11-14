84253b3c00b7b54ef9ab72a42f5892e39b33c380
rockchen <rochchen@htsc.com>
2023-11-14 14:44:43 +0800

    update

--- src/main/java/com/hthk/fintech/model/web/http/HttpResponse.java
+++ src/main/java/com/hthk/fintech/model/web/http/HttpResponse.java
@@ -6,5 +6,52 @@ package com.hthk.fintech.model.web.http;
  */
 public class HttpResponse<R> {
 
+    private HttpStatusCodeEnum statusCode;
+
+    private String message;
+
+    private R data;
+
+    public HttpResponse() {
+    }
+
+    public HttpResponse(HttpStatusCodeEnum statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public HttpResponse(HttpStatusCodeEnum statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public HttpResponse(HttpStatusCodeEnum statusCode, String message, R data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    public HttpStatusCodeEnum getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(HttpStatusCodeEnum statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public R getData() {
+        return data;
+    }
+
+    public void setData(R data) {
+        this.data = data;
+    }
 
 }
---
+++ src/main/java/com/hthk/fintech/model/web/http/HttpStatusCodeEnum.java
@@ -0,0 +1,37 @@
+package com.hthk.fintech.model.web.http;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/14 14:34
+ */
+public enum HttpStatusCodeEnum {
+
+    SUCCESS(200, "success"),
+
+    FAIL(400, "fail");
+
+    private int statusCode;
+
+    private String displayMessage;
+
+    HttpStatusCodeEnum(int statusCode, String displayMessage) {
+        this.statusCode = statusCode;
+        this.displayMessage = displayMessage;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getDisplayMessage() {
+        return displayMessage;
+    }
+
+    public void setDisplayMessage(String displayMessage) {
+        this.displayMessage = displayMessage;
+    }
+}
