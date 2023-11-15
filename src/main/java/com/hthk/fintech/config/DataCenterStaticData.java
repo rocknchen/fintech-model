4f6beb680579a0e80e7574c786e5becbf57aa285
rockchen <rochchen@htsc.com>
2023-11-15 18:26:41 +0800

    update

---
+++ src/main/java/com/hthk/fintech/config/DataCenterStaticData.java
@@ -0,0 +1,17 @@
+package com.hthk.fintech.config;
+
+import com.hthk.fintech.model.data.datacenter.query.SnapshotImageEnum;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/11/15 18:14
+ */
+public class DataCenterStaticData {
+
+    public static final List<SnapshotImageEnum> WHITE_LIST_FILE_SUPPORTED_IMAGE_TYPE
+            = Arrays.asList(SnapshotImageEnum.LATEST);
+
+}
