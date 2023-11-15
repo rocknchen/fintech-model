b60336302b84215385128ee78a597594233cacf6
rockchen <rochchen@htsc.com>
2023-11-15 15:46:07 +0800

    update

--- src/main/java/com/hthk/fintech/model/data/DataSourceFolder.java
+++ src/main/java/com/hthk/fintech/model/data/DataSourceFolder.java
@@ -2,6 +2,8 @@ package com.hthk.fintech.model.data;
 
 import com.hthk.fintech.model.data.basic.AbstractDataSource;
 
+import static com.hthk.fintech.model.data.DataSourceTypeEnum.*;
+
 /**
  * @Author: Rock CHEN
  * @Date: 2023/11/15 15:29
@@ -11,9 +13,11 @@ public class DataSourceFolder extends AbstractDataSource {
     private String path;
 
     public DataSourceFolder() {
+        type = FOLDER;
     }
 
     public DataSourceFolder(String path) {
+        this();
         this.path = path;
     }
 
