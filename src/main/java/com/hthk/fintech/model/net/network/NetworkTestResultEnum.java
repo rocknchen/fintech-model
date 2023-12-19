f19cac78586bd6b36f63f13af18930383f1c7d77
rockchen <rochchen@htsc.com>
2023-12-19 16:18:20 +0800

    update

---
+++ src/main/java/com/hthk/fintech/model/net/network/NetworkTestResultEnum.java
@@ -0,0 +1,11 @@
+package com.hthk.fintech.model.net.network;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/8 17:00
+ */
+public enum NetworkTestResultEnum {
+
+    SUCCESS, FAILED;
+
+}
---
+++ src/main/java/com/hthk/fintech/model/net/network/TelnetResult.java
@@ -0,0 +1,11 @@
+package com.hthk.fintech.model.net.network;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/8 16:57
+ */
+public class TelnetResult {
+
+    private NetworkTestResultEnum result;
+
+}
