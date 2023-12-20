21973c979fb3b0d9546a888bb8be89d8929fbaa8
rockchen <rochchen@htsc.com>
2023-12-20 08:38:37 +0800

    update

---
+++ src/main/java/com/hthk/fintech/model/basic/ICriteria.java
@@ -0,0 +1,8 @@
+package com.hthk.fintech.model.basic;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/19 20:44
+ */
+public interface ICriteria {
+}
---
+++ src/main/java/com/hthk/fintech/model/finance/trade/criteria/ICriteriaTrade.java
@@ -0,0 +1,10 @@
+package com.hthk.fintech.model.finance.trade.criteria;
+
+import com.hthk.fintech.model.basic.ICriteria;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/19 20:44
+ */
+public interface ICriteriaTrade extends ICriteria {
+}
