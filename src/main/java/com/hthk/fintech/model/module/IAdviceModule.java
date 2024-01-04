86c1e92b8845faa1425890c3692a3d4cecb62837
rockchen <rochchen@htsc.com>
2024-01-04 21:48:47 +0800

    update

--- src/main/java/com/hthk/fintech/model/module/IAdviceModule.java
+++ src/main/java/com/hthk/fintech/model/module/IAdviceModule.java
@@ -1,11 +1,13 @@
 package com.hthk.fintech.model.module;
 
+import com.hthk.fintech.exception.ServiceInternalException;
+
 /**
  * @Author: Rock CHEN
  * @Date: 2024/1/4 21:36
  */
 public interface IAdviceModule {
 
-    void before();
+    void before() throws ServiceInternalException;
 
 }
