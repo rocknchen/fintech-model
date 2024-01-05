cb68c64ae10eab862b4e45957f829b0b31d5aac5
rockchen <rochchen@htsc.com>
2024-01-05 09:10:07 +0800

    update

--- src/main/java/com/hthk/fintech/config/FintechStaticData.java
+++ src/main/java/com/hthk/fintech/config/FintechStaticData.java
@@ -40,6 +40,8 @@ public class FintechStaticData {
 
     public static final SimpleDateFormat DEFAULT_DATE_FORMAT_SDF = new SimpleDateFormat(BASIC_DATE_FORMAT);
 
+    public static final SimpleDateFormat DEFAULT_DATE_TIME_FORMAT_SDF = new SimpleDateFormat(DEFAULT_DATE_TIME_FORMAT);
+
     public static final String SIMPLE_DATE_TIME_FORMAT = "yyyyMMdd_HHmmss";
 
     public static final String KW_SAVE_EVENT = "SAVE EVENT";
