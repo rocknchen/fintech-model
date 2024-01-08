4519478320a445f1b3ef1ff95d4859a0cf340e2a
rockchen <rochchen@htsc.com>
2024-01-08 14:39:31 +0800

    update

---
+++ src/main/java/com/hthk/fintech/model/web/http/HttpServiceInstance.java
@@ -0,0 +1,28 @@
+package com.hthk.fintech.model.web.http;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2024/1/8 13:12
+ */
+public class HttpServiceInstance {
+
+    private String name;
+
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
