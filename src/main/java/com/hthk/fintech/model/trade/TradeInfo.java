01bb8f9dad995381de2a5aa50c7453209fdf2731
rockchen <rochchen@htsc.com>
2024-01-18 10:41:49 +0800

    update

--- src/main/java/com/hthk/fintech/model/trade/TradeInfo.java
+++ src/main/java/com/hthk/fintech/model/trade/TradeInfo.java
@@ -1,7 +1,9 @@
 package com.hthk.fintech.model.trade;
 
+import com.hthk.fintech.enumration.CSVField;
 import com.hthk.fintech.enumration.FieldOrder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -11,36 +13,116 @@ import java.time.LocalDateTime;
  */
 @FieldOrder({"book", "tradeId", "externalReference",
         "internalReference", "productType", "productSubType",
-        "productExtType", "trader", "futureUnderlyingTickerExchange",
-        "futureUnderlyingBBTickerExchange"})
+        "productExtType", "tradeDateTime", "settlementDate",
+        "buySell", "price", "quantity",
+        "tradeStatus", "trader", "enteredUser",
+        "enteredDateTime", "futureUnderlyingTickerExchange", "futureUnderlyingBBTickerExchange"})
 public class TradeInfo {
 
+    @CSVField(header = "book")
     private String book;
 
+    @CSVField(header = "tradeId")
     private String tradeId;
 
+    @CSVField(header = "externalReference")
     private String externalReference;
 
+    @CSVField(header = "internalReference")
     private String internalReference;
 
+    @CSVField(header = "productType")
     private String productType;
 
+    @CSVField(header = "productSubType")
     private String productSubType;
 
+    @CSVField(header = "productExtType")
     private String productExtType;
 
+    @CSVField(header = "tradeDateTime")
     private LocalDateTime tradeDateTime;
 
+    @CSVField(header = "settlementDate")
     private LocalDate settlementDate;
 
+    @CSVField(header = "buySell")
+    private String buySell;
+
+    @CSVField(header = "price")
+    private BigDecimal price;
+
+    @CSVField(header = "quantity")
+    private String quantity;
+
+    @CSVField(header = "tradeStatus")
+    private String tradeStatus;
+
+    @CSVField(header = "trader")
     private String trader;
+
+    @CSVField(header = "enteredUser")
+    private String enteredUser;
+
+    @CSVField(header = "enteredDateTime")
+    private LocalDateTime enteredDateTime;
 
     /**
      * FutureFX
      */
+    @CSVField(header = "futureUnderlyingTickerExchange")
     private String futureUnderlyingTickerExchange;
 
+    @CSVField(header = "futureUnderlyingBBTickerExchange")
     private String futureUnderlyingBBTickerExchange;
+
+    public String getBuySell() {
+        return buySell;
+    }
+
+    public void setBuySell(String buySell) {
+        this.buySell = buySell;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getTradeStatus() {
+        return tradeStatus;
+    }
+
+    public void setTradeStatus(String tradeStatus) {
+        this.tradeStatus = tradeStatus;
+    }
+
+    public String getEnteredUser() {
+        return enteredUser;
+    }
+
+    public void setEnteredUser(String enteredUser) {
+        this.enteredUser = enteredUser;
+    }
+
+    public LocalDateTime getEnteredDateTime() {
+        return enteredDateTime;
+    }
+
+    public void setEnteredDateTime(LocalDateTime enteredDateTime) {
+        this.enteredDateTime = enteredDateTime;
+    }
 
     public String getFutureUnderlyingBBTickerExchange() {
         return futureUnderlyingBBTickerExchange;
