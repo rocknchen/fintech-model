0f238e58c4fd487ee29ed06fba1612046b4d27a7
rockchen <rochchen@htsc.com>
2024-01-23 15:03:59 +0800

    update

--- src/main/java/com/hthk/fintech/model/web/http/HttpRequestGetParams.java
+++ src/main/java/com/hthk/fintech/model/web/http/HttpRequestGetParams.java
@@ -9,7 +9,7 @@ import static com.hthk.fintech.model.web.http.ActionTypeEnum.*;
  * @Author: Rock CHEN
  * @Date: 2023/12/21 9:40
  */
-@HttpRequestParams(names = {GET, COUNT, PRICE, SYNCHRONIZE})
+@HttpRequestParams(names = {GET, COUNT, PRICE, SYNCHRONIZE, COPY_ADD})
 public class HttpRequestGetParams implements ApplicationSourceParams {
 
     private ApplicationInstance source;
