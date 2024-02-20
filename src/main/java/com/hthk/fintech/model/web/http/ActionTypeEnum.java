4d9d12745a18195d9a067644e27d00aaa4418ed4
rockchen <rochchen@htsc.com>
2024-02-20 09:41:12 +0800

    update

--- src/main/java/com/hthk/fintech/model/web/http/ActionTypeEnum.java
+++ src/main/java/com/hthk/fintech/model/web/http/ActionTypeEnum.java
@@ -6,6 +6,6 @@ package com.hthk.fintech.model.web.http;
  */
 public enum ActionTypeEnum {
 
-    GET, COUNT, SYNCHRONIZE, PRICE, COPY_ADD, PROCESS, GENERATE;
+    GET, COUNT, SYNCHRONIZE, PRICE, COPY_ADD, PROCESS, GENERATE, AMEND;
 
 }
