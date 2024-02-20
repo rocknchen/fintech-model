584b9a6229710a799e5f129577635ed542cb0283
rockchen <rochchen@htsc.com>
2024-02-20 15:58:58 +0800

    update

---
+++ src/main/java/com/hthk/fintech/model/staticdata/BookInfo.java
@@ -0,0 +1,48 @@
+package com.hthk.fintech.model.staticdata;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2024/2/20 15:54
+ */
+public class BookInfo {
+
+    private String id;
+
+    private String name;
+
+    private String activity;
+
+    private String baseCurrency;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+}
