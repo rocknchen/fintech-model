474238f8d8b4eb2fa4f928800a99b397202f9a21
rockchen <rochchen@htsc.com>
2024-02-20 16:53:22 +0800

    update

--- src/main/java/com/hthk/fintech/model/staticdata/BookInfo.java
+++ src/main/java/com/hthk/fintech/model/staticdata/BookInfo.java
@@ -1,18 +1,72 @@
 package com.hthk.fintech.model.staticdata;
 
+import com.hthk.fintech.enumration.CSVField;
+import com.hthk.fintech.enumration.FieldOrder;
+
 /**
  * @Author: Rock CHEN
  * @Date: 2024/2/20 15:54
  */
+@FieldOrder({"id", "name", "activity",
+        "baseCurrency", "accessCurrency", "accessCurrencyPair",
+        "accessProduct", "testStatus"})
 public class BookInfo {
 
+    @CSVField(header = "id")
     private String id;
 
+    @CSVField(header = "name")
     private String name;
 
+    @CSVField(header = "activity")
     private String activity;
 
+    @CSVField(header = "baseCurrency")
     private String baseCurrency;
+
+    @CSVField(header = "accessCurrency")
+    private String accessCurrency;
+
+    @CSVField(header = "accessCurrencyPair")
+    private String accessCurrencyPair;
+
+    @CSVField(header = "accessProduct")
+    private String accessProduct;
+
+    @CSVField(header = "attr.TEST_STATUS")
+    private String testStatus;
+
+    public String getTestStatus() {
+        return testStatus;
+    }
+
+    public void setTestStatus(String testStatus) {
+        this.testStatus = testStatus;
+    }
+
+    public String getAccessCurrency() {
+        return accessCurrency;
+    }
+
+    public void setAccessCurrency(String accessCurrency) {
+        this.accessCurrency = accessCurrency;
+    }
+
+    public String getAccessCurrencyPair() {
+        return accessCurrencyPair;
+    }
+
+    public void setAccessCurrencyPair(String accessCurrencyPair) {
+        this.accessCurrencyPair = accessCurrencyPair;
+    }
+
+    public String getAccessProduct() {
+        return accessProduct;
+    }
+
+    public void setAccessProduct(String accessProduct) {
+        this.accessProduct = accessProduct;
+    }
 
     public String getId() {
         return id;
