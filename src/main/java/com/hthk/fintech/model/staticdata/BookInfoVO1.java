4478d8d193784a9b4abe26fdf97f28d9fc63ea33
rockchen <rochchen@htsc.com>
2024-02-29 20:57:00 +0800

    update

--- src/main/java/com/hthk/fintech/model/staticdata/BookInfoVO1.java
+++ src/main/java/com/hthk/fintech/model/staticdata/BookInfoVO1.java
@@ -7,18 +7,187 @@ import com.hthk.fintech.enumration.FieldOrder;
  * @Author: Rock CHEN
  * @Date: 2024/2/29 17:16
  */
-@FieldOrder({"id", "name",
-        "baseCurrency"})
+@FieldOrder({"legalEntity", "name", "id",
+        "tradingDesk", "businessType", "tradingType",
+        "business", "companyShortName", "department",
+        "system", "tradingDeskRisk", "riskTeam",
+        "riskSubTeam", "bbTraderPortfolio", "baseCurrency",
+        "location", "endOfDay", "holidays"})
 public class BookInfoVO1 {
 
-    @CSVField(header = "id")
-    private String id;
+    @CSVField(header = "legalEntity")
+    private String legalEntity;
 
     @CSVField(header = "name")
     private String name;
 
+    @CSVField(header = "id")
+    private String id;
+
+    @CSVField(header = "attr.Trading Desk")
+    private String tradingDesk;
+
+    @CSVField(header = "attr.业务类别")
+    private String businessType;
+
+    @CSVField(header = "attr.交易性质")
+    private String tradingType;
+
+    @CSVField(header = "attr.Business")
+    private String business;
+
+    @CSVField(header = "attr.公司简称")
+    private String companyShortName;
+
+    @CSVField(header = "attr.Department")
+    private String department;
+
+    @CSVField(header = "attr.系统")
+    private String system;
+
+    @CSVField(header = "attr.Trading Desk Risk")
+    private String tradingDeskRisk;
+
+    @CSVField(header = "attr.风险小组")
+    private String riskTeam;
+
+    @CSVField(header = "attr.风险次小组")
+    private String riskSubTeam;
+
+    @CSVField(header = "attr.BB_TRADER_PORTFOLIO")
+    private String bbTraderPortfolio;
+
     @CSVField(header = "baseCurrency")
     private String baseCurrency;
+
+    @CSVField(header = "Location")
+    private String location;
+
+    @CSVField(header = "End Of Day")
+    private String endOfDay;
+
+    @CSVField(header = "Holidays")
+    private String holidays;
+
+    public String getBbTraderPortfolio() {
+        return bbTraderPortfolio;
+    }
+
+    public void setBbTraderPortfolio(String bbTraderPortfolio) {
+        this.bbTraderPortfolio = bbTraderPortfolio;
+    }
+
+    public String getLegalEntity() {
+        return legalEntity;
+    }
+
+    public void setLegalEntity(String legalEntity) {
+        this.legalEntity = legalEntity;
+    }
+
+    public String getTradingDesk() {
+        return tradingDesk;
+    }
+
+    public void setTradingDesk(String tradingDesk) {
+        this.tradingDesk = tradingDesk;
+    }
+
+    public String getBusinessType() {
+        return businessType;
+    }
+
+    public void setBusinessType(String businessType) {
+        this.businessType = businessType;
+    }
+
+    public String getTradingType() {
+        return tradingType;
+    }
+
+    public void setTradingType(String tradingType) {
+        this.tradingType = tradingType;
+    }
+
+    public String getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(String business) {
+        this.business = business;
+    }
+
+    public String getCompanyShortName() {
+        return companyShortName;
+    }
+
+    public void setCompanyShortName(String companyShortName) {
+        this.companyShortName = companyShortName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getSystem() {
+        return system;
+    }
+
+    public void setSystem(String system) {
+        this.system = system;
+    }
+
+    public String getTradingDeskRisk() {
+        return tradingDeskRisk;
+    }
+
+    public void setTradingDeskRisk(String tradingDeskRisk) {
+        this.tradingDeskRisk = tradingDeskRisk;
+    }
+
+    public String getRiskTeam() {
+        return riskTeam;
+    }
+
+    public void setRiskTeam(String riskTeam) {
+        this.riskTeam = riskTeam;
+    }
+
+    public String getRiskSubTeam() {
+        return riskSubTeam;
+    }
+
+    public void setRiskSubTeam(String riskSubTeam) {
+        this.riskSubTeam = riskSubTeam;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEndOfDay() {
+        return endOfDay;
+    }
+
+    public void setEndOfDay(String endOfDay) {
+        this.endOfDay = endOfDay;
+    }
+
+    public String getHolidays() {
+        return holidays;
+    }
+
+    public void setHolidays(String holidays) {
+        this.holidays = holidays;
+    }
 
     public String getId() {
         return id;
